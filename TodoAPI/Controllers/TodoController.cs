fdcfe83363e2eb0b7a1fbbff3b84d2bf4833e09e
Max Gagner <m.gagner@hotmail.se>
2025-03-05 16:46:36 +0100

    added crud operations

--- TodoAPI/Controllers/TodoController.cs
+++ TodoAPI/Controllers/TodoController.cs
@@ -1,14 +1,129 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Models;
+using TodoApi.Data;
 
 namespace TodoAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class TodoController : ControllerBase
     {
+        private readonly AppDbContext _context;
 
+        public TodoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET: api/Todo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        //GET: api/Todo/id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(int id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        //POST: api/Todo
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> CreateTodoItem(TodoItem todoItem)
+        {
+            // Set created date to current time
+            todoItem.CreatedAt = DateTime.Now;
+            
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        }
+
+        //PUT: api/Todo/id
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTodoItem(int id, TodoItem todoItem)
+        {
+            if (id != todoItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoItem).State = EntityState.Modified;
+            
+            // Don't modify the creation date when updating
+            _context.Entry(todoItem).Property(x => x.CreatedAt).IsModified = false;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //DELETE: api/Todo/id
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(int id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        //like put but only changes one field instead of entire object, neat
+        //PATCH: api/Todo/5/complete
+        [HttpPatch("{id}/complete")]
+        public async Task<IActionResult> CompleteTodoItem(int id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            todoItem.IsCompleted = true;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoItemExists(int id)
+        {
+            return _context.TodoItems.Any(e => e.Id == id);
+        }
     }
 }
